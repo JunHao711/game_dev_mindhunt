477ab8d6d5f9093ea32eb44a5eb0138bdbe5070a
junhao <evon.chew77@gmail.com>
2025-07-05 23:53:25 +0800

    Added Healing and Damaging logic for player

--- Assets/Scripts/Player_health.cs
+++ Assets/Scripts/Player_health.cs
@@ -7,29 +7,89 @@ public class Player_health : MonoBehaviour
     public int current_health;
     public int max_health;
     public int damage;
+    public int healthPickupAmount = 1;
 
     public Health_bar healthbar;
+
+    public float immortalTime = 2f;
+    private float immortalCounter;
+
+    private SpriteRenderer spriteRenderer;
+    public float blinkInterval = 0.1f;
+
 
     // Start is called before the first frame update
     void Start()
     {
         current_health = max_health;
         healthbar.setMaxHealth(current_health);
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(immortalCounter > 0 )
+        {
+            immortalCounter -= Time.deltaTime;
+        }
     }
 
     public void GetDamage()
     {
-        current_health -= damage;
-        healthbar.setHealth(current_health);
-        if (current_health <= 0)
+        if(immortalCounter <= 0)
         {
-            gameObject.SetActive(false);
+            current_health -= damage;
+            healthbar.setHealth(current_health);
+            if (current_health <= 0)
+            {
+                gameObject.SetActive(false);
+            }
+            else
+            {
+                StartCoroutine(Blink());// reset
+            }
+        }
+    }
+
+    IEnumerator Blink()
+    {
+        immortalCounter = immortalTime;
+
+        while (immortalCounter > 0)
+        {
+            spriteRenderer.enabled = false;
+            yield return new WaitForSeconds(blinkInterval);
+            spriteRenderer.enabled = true;
+            yield return new WaitForSeconds(blinkInterval);
+
+            immortalCounter -= blinkInterval * 2;
+        }
+
+        spriteRenderer.enabled = true;
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Immortal_Potion")
+        {
+            immortalCounter = immortalTime;
+            Destroy(collision.gameObject);
+            //immortalEffect.SetActive(true);
+        }
+        if (collision.gameObject.tag == "Healing_Potion")
+        {
+            current_health += healthPickupAmount;
+            healthbar.setHealth(current_health);
+
+            if (current_health > max_health)
+            {
+                current_health = max_health;
+            }
+
+            Destroy(collision.gameObject);
         }
     }
 }
