ec05cd955bc9e8d947cacff642bf0a59dd9f447d
JunHao711 <evon.chew77@gmail.com>
2025-08-30 01:10:21 +0800

    Implemented the CeilingDropper and Cannon Trap

---
+++ Assets/Scripts/Dropper.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dropper : MonoBehaviour
+{
+    public GameObject rockPrefab;
+    public Transform spawnPoint;
+    public float interval = 2.5f;
+    public Vector2 randomJitter = new Vector2(-0.4f, 0.4f);
+
+    IEnumerator Start()
+    {
+        yield return null;
+        while (true)
+        {
+            Instantiate(rockPrefab, spawnPoint ? spawnPoint.position : transform.position, Quaternion.identity);
+            float t = interval + Random.Range(randomJitter.x, randomJitter.y);
+            yield return new WaitForSeconds(Mathf.Max(0.1f, t));
+        }
+    }
+}
---
+++ Assets/Scripts/canoonShooter.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class canoonShooter : MonoBehaviour
+{
+    public GameObject rockPrefab;
+    public Transform muzzle;
+    public float interval = 1.5f;
+    public float speed = 10f;
+    public bool useGravityArc = false;
+    public bool onlyWhenPlayerInRange = true;
+    public float detectRange = 8f;
+    public LayerMask playerMask;
+
+    Rigidbody2D rbTemp;
+
+    IEnumerator Start()
+    {
+        if (!rockPrefab || !muzzle)
+        {
+            Debug.LogError("CannonShooter: rockPrefab 或 muzzle 未设置", this);
+            yield break;
+        }
+        if (rockPrefab.scene.IsValid())
+        {
+            Debug.LogError("CannonShooter: rockPrefab 指向场景对象，请拖入 Project 面板里的 prefab 资产", rockPrefab);
+            yield break;
+        }
+
+        while (true)
+        {
+            if (!onlyWhenPlayerInRange || PlayerInFront())
+                Shoot();
+
+            yield return new WaitForSeconds(interval);
+        }
+    }
+
+    bool PlayerInFront()
+    {
+        var hit = Physics2D.OverlapCircle(muzzle.position, detectRange, playerMask);
+        return hit != null;
+    }
+
+    void Shoot()
+    {
+        var go = Instantiate(rockPrefab, muzzle.position, muzzle.rotation);
+        var rb = go.GetComponent<Rigidbody2D>();
+        if (rb)
+        {
+            rb.gravityScale = useGravityArc ? Mathf.Max(1f, rb.gravityScale) : 0f;
+            rb.velocity = (Vector2)muzzle.right * speed;
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (muzzle)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawRay(muzzle.position, muzzle.right * 0.6f);
+            Gizmos.DrawWireSphere(muzzle.position, detectRange);
+        }
+    }
+}
